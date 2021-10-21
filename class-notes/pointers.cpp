a9715223c0e2aae6f379b4f581ee503bb6249c71
xiaogavin-hunter <gavin.xiao89@myhunter.cuny.edu>
2021-10-21 10:51:03 -0400

    added pointer notes

---
+++ class-notes/pointers.cpp
@@ -0,0 +1,21 @@
+/*
+Pointers Notes 10/21
+
+
+*/
+
+#include <iostream>
+
+int main() {
+    // int *p = nullptr; 
+    // int p_int = 10; 
+
+    // std::cout << "address of p: " << &p << std::endl; 
+
+    // p = &p_int; 
+    // std::cout << "value of p: " << p_int << std::endl;
+    // std::cout << "address of p after pointing: " << &p << std::endl; 
+
+
+    return 0;
+}
