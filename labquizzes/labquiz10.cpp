f25e0497f98eacd8af052f0de903bedb6c62eab5
xiaogavin-hunter <gavin.xiao89@myhunter.cuny.edu>
2021-12-09 08:02:59 -0500

    Completed

--- labquizzes/labquiz10.cpp
+++ labquizzes/labquiz10.cpp
@@ -1,1 +1,20 @@
-// haha this file doesn't exist :D
+/*
+Author: Gavin Xiao
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Lab Quiz 10
+
+Image is a checkboard pattern, 
+alt between black and white, only has the code for the loop, 
+*/
+
+// for(int row = 0; row < h; row++) { 
+    // for int col = 0; col < w; col++) { 
+        // implementation 
+        if(col % 2 == 1 && row % 2 == 0 || col % 2 == 0 && row % 2 == 1) { 
+            out[row][col] = img[row][col]; 
+        } else { 
+            out[row][col] = 0;
+        }
+    // }
+// }
